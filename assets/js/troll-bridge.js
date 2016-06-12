d2e50f2ecec41bfe7ac0e24525b4c3a777b9c697
Cecile Nguyen <cecile.ceng@gmail.com>
2016-06-12 16:26:31 -0500

    commenting out code

--- assets/js/troll-bridge.js
+++ assets/js/troll-bridge.js
@@ -32,15 +32,15 @@ $('#search').on('click', function() {
 
 
 
-var text = "Don King, and so many other African Americans who know me well and endorsed me, would not have done so if they thought I was a racist!"
-var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=" + text + "&highlight_expression=links&apikey=e91f7825-eb95-4ef2-be5e-09485475e3d8";
+// var text = "Don King, and so many other African Americans who know me well and endorsed me, would not have done so if they thought I was a racist!"
+// var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=" + text + "&highlight_expression=links&apikey=e91f7825-eb95-4ef2-be5e-09485475e3d8";
 
-$.ajax({
-    url: queryURL,
-    method: 'GET'
-    })
+// $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+//     })
 
-    .done(function(response) {
-        console.log(response)
+//     .done(function(response) {
+//         console.log(response)
         
-    });
+//     });
