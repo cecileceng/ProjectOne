5c2c538a4dca8171a550c924479c0abb847f4b52
Cecile Nguyen <cecile.ceng@gmail.com>
2016-06-13 12:47:38 -0500

    more ui adjustments

--- assets/js/troll-bridge.js
+++ assets/js/troll-bridge.js
@@ -47,8 +47,6 @@ $('#search').on('click', function() {
  //    });
 }) 
 
-
-
 var text = "Don King, and so many other African Americans who know me well and endorsed me, would not have done so if they thought I was a racist!"
 var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=" + text + "&highlight_expression=links&apikey=e91f7825-eb95-4ef2-be5e-09485475e3d8";
 
