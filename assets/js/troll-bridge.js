90219093fc73bc9383f7efceb9268811ea53e120
Cecile Nguyen <cecile.ceng@gmail.com>
2016-06-15 20:43:31 -0500

    BREAKTHROUGH we got the text to quit being dumb and cleaned out any weird nonwords

--- assets/js/troll-bridge.js
+++ assets/js/troll-bridge.js
@@ -14,9 +14,9 @@ $('#search').on('click', function() {
             var tweetText = [];
             console.log(data);
 
-            for (i = 0; i < 5; i++) {
+            for (i = 0; i < 20; i++) {
 
-//                if(data[i].retweeted == true) {data.splice(i,1);}
+                if(data[i].retweeted == true) {data.splice(i,1);}
                 data[i].entities = false
                 data[i].extended_entities = false
             tweetText.push(data[i].text);
@@ -35,8 +35,13 @@ $('#search').on('click', function() {
         var cleanText = totalTweet.replace(handle_regex, '').replace(hashtag_regex, '').replace(url_www_regex, '').replace(url_other_regex, '').replace(colon_regex, '').replace(line_break_regex, '').replace(RT_regex, '');
         JSON.stringify(cleanText);
         console.log(cleanText);
-        //var cleanText = "The Legend of Zelda Retweet event begins!  in-app gifts for s! ⇒   h…. Technology catching up with our imaginations! Here's a staup with man-made shooting stars in its eyes  via .  Also, to clarify, not actually rich.. Inspiring words from Madeleine Albright in a commencement speech for the class of 2016 at Scripps     Looking forward to rich people problems  "
-        var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=" + cleanText + "&highlight_expression=links&apikey=e91f7825-eb95-4ef2-be5e-09485475e3d8";    
+        var cleaner_cleaner = /\W/gim;
+        var cleanTextTwo = cleanText.replace(cleaner_cleaner, ' ');
+        console.log(cleanTextTwo)
+        var uri = cleanTextTwo;
+        var res = encodeURI(uri);
+//        var cleanText = "The Legend of Zelda Retweet event begins!  in-app gifts for s! h…. Technology catching up with our imaginations! Here's a staup with man-made shooting stars in its eyes  via .  Also, to clarify, not actually rich.. Inspiring words from Madeleine Albright in a commencement speech for the class of 2016 at Scripps     Looking forward to rich people problems  "
+        var queryURL = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text=" + uri + "&highlight_expression=links&apikey=e91f7825-eb95-4ef2-be5e-09485475e3d8";    
         //$(".tweets").append(cleanText);
         //$(".tweets").append("<p>hello</p>");
         
